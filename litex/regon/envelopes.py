03529a06a60960e76a269379ce435d5e1d5bd2f5
kicaj <kicaj@kdev.pl>
2020-11-03 13:03:11 +0100

    Update envelopes.py
    Results of `DaneSzukajPodmioty` is the same as `DaneSzukaj`, but had more necessary fields, like: `Nip`, `NrNieruchomosci`, `NrLokalu`, `DataZakonczeniaDzialalnosci` and `MiejscowoscPoczty` and only do not have `RegonLink` field.

--- litex/regon/envelopes.py
+++ litex/regon/envelopes.py
@@ -64,14 +64,14 @@ SEARCH_ENVELOPE = '''\
 <soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope" xmlns:ns="http://CIS/BIR/PUBL/2014/07" xmlns:dat="http://CIS/BIR/PUBL/2014/07/DataContract">
 <soap:Header xmlns:wsa="http://www.w3.org/2005/08/addressing">
 <wsa:To>{api.service_url}</wsa:To>
-<wsa:Action>http://CIS/BIR/PUBL/2014/07/IUslugaBIRzewnPubl/DaneSzukaj</wsa:Action>
+<wsa:Action>http://CIS/BIR/PUBL/2014/07/IUslugaBIRzewnPubl/DaneSzukajPodmioty</wsa:Action>
 </soap:Header>
    <soap:Body>
-      <ns:DaneSzukaj>
+      <ns:DaneSzukajPodmioty>
          <ns:pParametryWyszukiwania>
             {param}
          </ns:pParametryWyszukiwania>
-      </ns:DaneSzukaj>
+      </ns:DaneSzukajPodmioty>
    </soap:Body>
 </soap:Envelope>
 '''
